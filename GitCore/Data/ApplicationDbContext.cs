46986036037f7465748ed3470a6a6a88b1dee701
iDhruvi <dhruvimodi.dcs22n@vnsgu.ac.in>
2022-10-19 22:59:48 +0530

    Added new changes for project structure.

---
+++ GitCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using GitCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GitCore.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employee { get; set; }
+}
