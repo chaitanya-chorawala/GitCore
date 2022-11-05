75102f3e0144b44a1578a3624201aefcaff23cf2
iDhruvi <dhruvimodi.dcs22n@vnsgu.ac.in>
2022-11-05 14:04:36 +0530

    Added get employee

---
+++ GitCore/Controllers/EmployeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetEmployees()
+        {
+            return Ok();
+        }
+    }
+}
